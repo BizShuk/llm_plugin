bc6c0b81fb67662f4546df56ac0a077c91889bee
shuk <biz.shuk@gmail.com>
2026-01-31 00:47:29 +0800

    Update

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+
+mkdir -p ~/.gemini/antigravity
+mkdir -p ~/.gemini/rules
+mkdir -p ~/.gemini/skills
+mkdir -p ~/.gemini/workflows
+
+ln -s ~/.gemini/skills ~/.gemini/antigravity/global_skills
+ln -s ~/.gemini/workflows ~/.gemini/antigravity/global_workflows
+
 ln -s $(pwd)/rules/GEMINI.global.md ~/.gemini/GEMINI.md
